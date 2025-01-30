dd6c9a714a27819e508d3508dadd143bb257715d
mmasharov <masharov.m@gmail.com>
2025-01-30 12:21:16 +0500

    SmartForms Методы получения элементов, каталогов и уровней измерения

--- api/services/SmartForms.py
+++ api/services/SmartForms.py
@@ -20,6 +20,33 @@ class SmartForms(ApiRequest):
         self.__response = super().sendRequest().json()
         return self.__response
     
+    def getDimElements(self, dimName:str):
+        '''Метод получения элементов измерения'''
+        self.__endpoint = f'smart-forms/api/dimensions/{dimName}/elements'
+        headers = self.__headers
+        headers['x-api-version'] = '2.0'
+        super().__init__('get', self.__protocol, self.__address, self.__cert, endpoint=self.__endpoint, headers=headers)
+        self.__response = super().sendRequest().json()
+        return self.__response
+    
+    def getDimFolders(self, dimName:str):
+        '''Метод получения каталогов измерения'''
+        self.__endpoint = f'smart-forms/api/dimensions/{dimName}/folders'
+        headers = self.__headers
+        headers['x-api-version'] = '2.0'
+        super().__init__('get', self.__protocol, self.__address, self.__cert, endpoint=self.__endpoint, headers=headers)
+        self.__response = super().sendRequest().json()
+        return self.__response
+    
+    def getDimLevels(self, dimName:str):
+        '''Метод получения уровней измерения'''
+        self.__endpoint = f'smart-forms/api/dimensions/{dimName}/levels'
+        headers = self.__headers
+        headers['x-api-version'] = '2.0'
+        super().__init__('get', self.__protocol, self.__address, self.__cert, endpoint=self.__endpoint, headers=headers)
+        self.__response = super().sendRequest().json()
+        return self.__response
+    
     def addDimElement(self, dimName:str, data:list):
         '''Метод добавления элемента в измерение'''
         self.__endpoint = f'smart-forms/api/dimensions/{dimName}/elements'
